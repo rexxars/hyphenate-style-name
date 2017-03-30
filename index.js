d688b8cfc1b897daa8776eb0ab6c0b41aef06e9d
Jas Chen <jas228@gmail.com>
2017-03-30 12:54:03 +0800

    Faster implementation

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ var uppercasePattern = /[A-Z]/g;
 var msPattern = /^ms-/;
 var cache = {};
 
-function hyphenateStyleName(string) {
-    return string in cache
-    ? cache[string]
-    : cache[string] = string
-      .replace(uppercasePattern, '-$&')
-      .toLowerCase()
-      .replace(msPattern, '-ms-');
+function toHyphenLower(match) {
+    return '-' + match.toLowerCase();
+}
+
+function hyphenateStyleName(name) {
+    if (cache.hasOwnProperty(name)) {
+        return cache[name];
+    }
+
+    var hName = name.replace(uppercasePattern, toHyphenLower);
+
+    return cache[name] = msPattern.test(hName) ? '-' + hName : hName;
 }
 
 module.exports = hyphenateStyleName;
